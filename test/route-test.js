2333ab6bcf82c229c18639f9f8dec9569670c61d
whaangbuu <chardy.orofeo@yahoo.com.ph>
2017-11-04 00:10:20 +0800

    Added passing tests for index route

--- test/route-test.js
+++ test/route-test.js
@@ -1,2 +1,26 @@
-var chai = require("chai");
+var chai = require('chai');
+var should = chai.should;
 var expect = chai.expect;
+var chaiHttp = require('chai-http');
+
+var app = require("../app");
+
+chai.use(chaiHttp)
+
+describe('User Routes', function () {
+    it('error should be null on index route', function () {
+        chai.request(app)
+            .get('/')
+            .end(function (err, res) {
+                expect(err).to.be.null
+            })
+    });
+
+    it('should have a status code of 200 on index route', function () {
+        chai.request(app)
+            .get('/')
+            .end(function (err, res) {
+                expect(res).to.have.status(200)
+            })
+    })
+})
