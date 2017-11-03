809b0b0bcfd14c8b9e3d98a9aaa5009c53f3cf2e
Kentoy <jankenith.johannessen@gmail.com>
2017-11-03 23:22:23 +0800

    Initial YOLO commit, no NPM install :poop:

---
+++ controllers/news.js
@@ -0,0 +1,76 @@
+var Twitter = require('twitter-node-client').Twitter;
+var request = require('request');
+var variables = require('../models');
+var RssFeedEmitter = require('rss-feed-emitter');
+
+var twitter = new Twitter(variables.twitterConfig);
+
+var feeder = InitializeFeeder(new RssFeedEmitter(), variables.feeds);
+
+
+function InitializeFeeder(feeder, feeds) {
+    for (var i = 0; i < feeds.length; i++) {
+        feeder.add(variables.feeds[i]);
+    }
+
+    feeder.on('new-item', function (item) {
+        // console.log(item.meta.title);
+        console.log('=================================================');
+        console.log(new Date(Date.now()));
+        console.log(item.title);
+        console.log(item.description);
+        console.log(item.link);
+        let name = item.link.split('/').slice(-1)[0];
+        console.log(name);
+        checkIfExist(name, item, item.meta.title.replace(/[\. ,:-]+/g, ''));
+    });
+
+    return feeder;
+}
+
+function checkIfExist(name, item, newspaper) {
+    var options = {
+        method: 'GET',
+        uri: variables.firebaseUrl + newspaper + '/' + name + '.json'
+    };
+
+    request(options, function (error, response, body) {
+        // console.log(body);
+        if (JSON.parse(body) == null) {
+            // Send Tweet if not found in Firebase    
+            sendTweet(name, item, newspaper);
+        }
+    });
+}
+
+function sendTweet(name, item, newspaper) {
+    twitter.postTweet({
+            status: item.link
+        },
+        function () {
+
+        },
+        function () {
+            console.log("Success!");
+            // Insert to Firebase if successful
+            sendPutRequest(name, item, newspaper);
+        });
+}
+
+
+function sendPutRequest(name, item, newspaper) {
+    var obj = {
+        description: item.description,
+        link: item.link,
+        title: item.title
+    }
+    var options = {
+        method: 'PUT',
+        url: variables.firebaseUrl + newspaper + '/' + name + '.json',
+        json: obj
+    };
+
+    request(options, function (error, response, body) {
+        // DO SOMETHING
+    });
+}
---
+++ models/index.js
@@ -0,0 +1,35 @@
+var models = (function(){
+
+    const feeds = [
+        {
+            url: 'http://www.sunstar.com.ph/rss/cebu',
+            refresh: 2000
+        },
+        {
+            url: 'http://cebudailynews.inquirer.net/feed',
+            refresh: 2000
+        },
+        {
+            url: 'http://www.philstar.com/rss/cebu-news',
+            refresh: 2000
+        }
+    ];
+
+    const twitterConfig = {
+        "consumerKey": "BZhVyAJwibXroymBCW2eCcHgE",
+        "consumerSecret": "SybIpu7hoDkXAfi4DEbD09kaQcO2tLo8AHjeOoy33xyGIH300v",
+        "accessToken": "926296647252037632-wXHwIb6iT0EKrtlKlPTEnnfORW1o0S3",
+        "accessTokenSecret": "74KuzuoitkeFst2qHzslZtNqRt8wKgDeMJv1qXo3blqxo",
+        "callBackUrl": ""
+    };
+
+    const firebaseUrl = 'https://pinggu-manjuu.firebaseio.com/news/';
+
+    return {
+        feeds: feeds,
+        twitterConfig: twitterConfig,
+        firebaseUrl: firebaseUrl
+    }
+})();
+
+module.exports = models;
